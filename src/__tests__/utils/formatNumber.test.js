791aa74e25e57613a0dccfc49de6f61305190ea9
Raul Bernardo Torta Monteiro <raul.monteiro@ciahering.com.br>
2023-01-28 11:13:05 -0300

    test: update util tests

--- src/__tests__/utils/formatNumber.test.js
+++ src/__tests__/utils/formatNumber.test.js
@@ -1,13 +1,41 @@
 import { formatPercent, formatCurrency } from "../../utils";
 
 describe("Format Percent Function", () => {
-  it("should return in percent format ", () => {
+  it("should return value in percentage format", () => {
     expect(formatPercent(0.5)).toEqual("50%");
+  });
+
+  it("should return value in percentage format", () => {
+    expect(formatPercent(0.555)).toEqual("55.5%");
+  });
+
+  it("should return value in percentage format", () => {
+    expect(formatPercent(0.56897)).toEqual("56.9%");
   });
 });
 
 describe("Format Currency Function", () => {
   test("should return value in currency format ", () => {
     expect(formatCurrency(150)).toEqual("$ 150.00");
+  });
+
+  test("should return value in currency format ", () => {
+    expect(formatCurrency(150.0)).toEqual("$ 150.00");
+  });
+
+  test("should return value in currency format ", () => {
+    expect(formatCurrency(150.59)).toEqual("$ 150.59");
+  });
+
+  test("should return value in currency format ", () => {
+    expect(formatCurrency(150.002)).toEqual("$ 150.00");
+  });
+
+  test("should return value in currency format ", () => {
+    expect(formatCurrency(150.599)).toEqual("$ 150.60");
+  });
+
+  test("should return value in currency format ", () => {
+    expect(formatCurrency(150.594)).toEqual("$ 150.59");
   });
 });
